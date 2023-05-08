6668d7a02437161c4b2545a17fb47a7d4e2e3e81
Yu-Chia Chen <yuchiachen99@gmail.com>
2023-05-08 18:17:19 -0500

    Add required tags to fields

--- backend/models/user.go
+++ backend/models/user.go
@@ -2,11 +2,10 @@ package models
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
-// User is a struct that represents a user in the database
 type User struct {
 	ID          primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
-	Username    string             `bson:"username" json:"username" unique:"true"`
+	Username    string             `bson:"username" json:"username" unique:"true" validate:"required"`
 	DisplayName string             `bson:"display_name,omitempty" json:"display_name,omitempty"`
-	Salt        string             `bson:"salt" json:"salt"`
-	Password    string             `bson:"password" json:"password"`
+	Salt        string             `bson:"salt" json:"salt" validate:"required"`
+	Password    string             `bson:"password" json:"password" validate:"required"`
 }
